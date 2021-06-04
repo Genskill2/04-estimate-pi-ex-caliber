05ffd105e64f2e4f4e4cc3a31a28c855f2beb4ba
ex-caliber <84794889+ex-caliber@users.noreply.github.com>
2021-06-04 12:20:45 +0530

    Monte_carlo_new

--- monte_carlo.c
+++ monte_carlo.c
@@ -11,7 +11,6 @@ float frandom() {
 float mc_pi(int n)
 {	
 	int pc=0,ps=n;
-	
 	for(int i=0; i<n; i++){
 	float x,y,len;
 	x=frandom();
@@ -24,29 +23,21 @@ float mc_pi(int n)
 	float pi = 4.0*pc/ps;
 	return pi;
 }
-
-
-
-int main(void) {
+  int main(void) {
   float pi0;
   float pi1;
-  
   pi0 = mc_pi(25000);
   pi1 = mc_pi(25000);
   printf("%f %f\n", pi0, pi1);
-  
   if (pi0 == pi1) {
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
       abort();
     }
-
-  if (fabs(pi0 - pi1) > 0.05) {
+    if (fabs(pi0 - pi1) > 0.05) {
       printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
       abort();
   }
-
-    
-  for (int i=2000; i<5000; i++) {
+    for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
