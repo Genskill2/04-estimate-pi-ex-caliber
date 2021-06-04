5092efd5bded79d6eb4c0c46e30e1c7e619b1031
ex-caliber <84794889+ex-caliber@users.noreply.github.com>
2021-06-04 12:31:42 +0530

    Monte carlo-1

--- monte_carlo.c
+++ monte_carlo.c
@@ -23,6 +23,7 @@ float mc_pi(int n)
 	float pi = 4.0*pc/ps;
 	return pi;
 }
+
   int main(void) {
   float pi0;
   float pi1;
@@ -33,10 +34,12 @@ float mc_pi(int n)
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
       abort();
     }
-    if (fabs(pi0 - pi1) > 0.05) {
+
+      if (fabs(pi0 - pi1) > 0.05) {
       printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
       abort();
   }
+
     for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
