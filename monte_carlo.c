dcda6307f5edd29499837b484831d71731d6ead9
ex-caliber <84794889+ex-caliber@users.noreply.github.com>
2021-06-04 12:14:14 +0530

    Monte_carlo_new

--- monte_carlo.c
+++ monte_carlo.c
@@ -2,14 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-float mc_pi(int);
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
+
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
+}
+
+
 
 int main(void) {
   float pi0;
@@ -38,6 +54,3 @@ int main(void) {
     }
   }
 }
-
-
-
